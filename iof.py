6fc2a31f76de3ace611d91d444d50cf789cdbdc3
Danya0x07 <dlef0xf8@gmail.com>
2020-06-26 13:46:03 +0300

    Решение для Иосифа какого-то там про воинов дегенератов.

---
+++ iof.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2020 Daniel Efimenko
+#
+
+n, m = map(int, input().split())
+
+warriors = [i + 1 for i in range(n)]
+next_to_kill = m
+
+while True:
+    del warriors[next_to_kill % len(warriors)]
+    if len(warriors) == 1:
+        break
+    next_to_kill += m
+
+print(warriors)
